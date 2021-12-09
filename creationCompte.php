c0475f53265a6a411064dd2c452633c6c80b5ba6
mayo <58669575+mayojuice@users.noreply.github.com>
2021-12-09 08:26:20 +0100

    Update creationCompte.php

--- creationCompte.php
+++ creationCompte.php
@@ -12,17 +12,19 @@
     <body>
         <form action = "creationComptePost.php" method = "post">
             <h3>Créer un compte</h3>
-            <label for = "nom">Nom : </label></br>
-            <input type = "text" id = "nom" name = "nom"></br>
             <label for = "email">Email : </label></br>
             <input type = "text" id = "email" name = "email"></br>
-            <label for = "nom">Mot de passe : </label></br>
+            <label for = "password">Mot de passe : </label></br>
             <input type = "password" id = "motDePasse" name = "motDePasse"></br>         
-            <label for = "nom">Adresse : </label></br>
+            <label for = "nom">Nom : </label></br>
+            <input type = "text" id = "nom" name = "nom"></br>
+            <label for = "nom">Prénom : </label></br>
+            <input type = "text" id = "nom" name = "nom"></br>
+            <label for = "adresse">Adresse : </label></br>
             <textarea id = "adresse" name = "adresse" rows = "3" cols = "16"></textarea></br>            
-            <label for = "nom">Ville : </label></br>
+            <label for = "ville">Ville : </label></br>
             <input type = "text" id = "ville" name = "ville"></br>
-            <label for = "nom">Téléphone : </label></br>
+            <label for = "telephone">Téléphone : </label></br>
             <input type = "text" id = "telephone" name = "telephone"></br>
             <input type="submit" value = "OK">
         </form>
