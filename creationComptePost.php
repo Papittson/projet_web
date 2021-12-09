7c425ed9d14be3b04d1f8f553e5c4ad25d0203ad
mayo <58669575+mayojuice@users.noreply.github.com>
2021-12-09 08:24:16 +0100

    Update creationComptePost.php

--- creationComptePost.php
+++ creationComptePost.php
@@ -1,31 +1,26 @@
 <html>
-<style>
-        body {
-            text-align: center;
-            font-family: Calibri;
-        }
-
-    </style>
 <body>
 <?php
-        $dsn = 'mysql:host=localhost:3306;dbname=technoweb;charset=UTF8'; //dsn pour Data Source Name, la chaine de cararctere indique ou est le SGBD, encodage utf8
+        $dsn = 'mysql:host=localhost:3306;dbname=technoweb;charset=UTF8';
         $username = 'root';
         $password = '';
         $dbh = new PDO($dsn, $username, $password) or die("Pb de connexion !");
-
-        $sth = $dbh->prepare("(INSERT INTO clients ('nom', 'email', 'password', 'ville', 'adresse', 'telephone)
-        VALUES ($nom, $email, $password, $ville, $adresse, $telephone);")
-        $sth->execute(array(
-            $nom = $_POST["nom"]
-            $email =  $_POST["email"]
-            $ville = $_POST["ville"]
-            $adresse =  $_POST["adresse"]
-            $telephone = $_POST["telephone"] ));
         
+        $sth->execute(array(
+            $email =  $_POST["email"],
+            $motDePasse = $email =  $_POST["motDePasse"],
+            $nom = $_POST["nom"],
+            $prenom = $_POST["nom"],
+            $ville = $_POST["ville"],
+            $adresse =  $_POST["adresse"],
+            $telephone = $_POST["telephone"] ));
+
+        $sth = $dbh->prepare("(INSERT INTO clients ('email', 'motDePasse', 'nom', 'prenom', 'ville', 'adresse', 'telephone)
+        VALUES (".$email.", ".$motDePasse.", ".$nom.", "$.prenom.", ".$ville.", ".$adresse.", ".$telephone.");");
         ?>
 
-
-Welcome <?php echo $_POST["nom"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+<!-- TEST -->
+Name: <?php echo $_POST["nom"]; ?><br>
+Email address: <?php echo $_POST["email"]; ?>
 </body>
 </html> 
