ec02a32f17cb093f7a82078bfef0dd8d07228ecf
mayo <farasyaninda@gmail.com>
2021-12-12 20:51:06 +0100

    Update panier.php

--- panier.php
+++ panier.php
@@ -27,6 +27,7 @@ $dbh = new PDO($dsn, $username, $password) or die("Pb de connexion !");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panier</title>
     <link rel="stylesheet" href="stylesheet.css" />
+    <script src="fonctions.js"></script>
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
